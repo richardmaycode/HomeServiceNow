c34e1def5d0db25a91d878b341625521ed25fa2c
Richard Wise <richard.wise@hey.com>
2023-02-21 18:31:26 -0500

    Initial Commit

---
+++ HomeServiceNow/HomeServiceNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeServiceNowApp.swift
+//  HomeServiceNow
+//
+//  Created by Richard Wise on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeServiceNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
